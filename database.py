a8da7a70b8b216488646f67b4cac10b949ed578c
Gennady Badinov <gennady.badinov@gmail.com>
2021-10-31 16:46:58 +0300

    TUI implementation started

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 import os
 import dotenv
-from typing import List, NewType
+from typing import List
 from sqlcipher3 import dbapi2 as sqlite3
-from datetime import datetime
-
+from faker import Faker
+from password_generator import PasswordGenerator
 
 """
 ------------------
@@ -24,9 +24,10 @@ def create_db(path: str, password: str) -> None:
         table acoounts:
             columns:
             1. id INTEGER PRIMARY KEY
-            2. url TEXT
-            3. username TEXT
-            4. password TEXT
+            2. item TEXT
+            3. url TEXT
+            4. username TEXT
+            5. password TEXT
     """
     connection = sqlite3.connect(path)
     if password is not None:
@@ -35,6 +36,7 @@ def create_db(path: str, password: str) -> None:
     cursor.execute(
         """CREATE TABLE IF NOT EXISTS accounts (
         id INTEGER PRIMARY KEY,
+        item TEXT,
         url TEXT,
         username TEXT,
         password TEXT
@@ -51,21 +53,31 @@ class DatabaseEngine:
         if password is not None:
             self.cursor.execute(f"pragma key={password}")
 
-    def safe_quit(self):
+    def safe_push(self):
         self.connection.commit()
-        self.connection.close()
+        # self.connection.close()
 
     def add_account(self, account: Account) -> None:
         self.cursor.execute(
-            "INSERT INTO accounts (url, username, password) VALUES (:url, :username, :password)",
+            "INSERT INTO accounts (item, url, username, password) VALUES (:item, :url, :username, :password)",
             {
+                "item": account.item,
                 "url": account.url,
                 "username": account.username,
                 "password": account.password,
             },
         )
 
-    def get_all_accounts(self) -> List:
+    def get_account(self, _item: str) -> Account:
+        # self.cursor.execute("SELECT * FROM accounts WHERE url=:url;", {"url": _url})
+        # self.cursor.execute("SELECT * FROM accounts WHERE item LIKE ?", _item)
+        self.cursor.execute(
+            "SELECT * FROM accounts WHERE item like '%{0}%'".format(_item)
+        )
+
+        return Account(*self.cursor.fetchone()[1:])  # [1:] -> without ID
+
+    def get_all_accounts(self) -> list:
         self.cursor.execute("SELECT * FROM accounts")
         return self.cursor.fetchall()
 
@@ -73,6 +85,7 @@ class DatabaseEngine:
         self.cursor.execute(
             "UPDATE accounts SET password=:password WHERE url=:url AND username=:username",
             {
+                "item": account.item,
                 "url": account.url,
                 "username": account.username,
                 "password": password,
@@ -87,30 +100,53 @@ class DatabaseEngine:
 
 
 class Account:
-    def __init__(self, url: str, username: str, password: str):
+    def __init__(self, item: str, url: str, username: str, password: str):
+        self.item = item
         self.url = url
         self.username = username
         self.password = password
 
+    @classmethod
+    def from_faker(cls):
+        faker = Faker()
+        item = faker.company()
+        url = faker.url()
+        username = faker.safe_email()
+        password = PasswordGenerator("random", 8).generate_password()
+        return cls(item, url, username, password)
+
     def __repr__(self) -> str:
-        return f"Account(url={self.url}, username={self.username}, password={self.password})"
+        return f"Account(item={self.item}, url={self.url}, username={self.username}, password={self.password})"
 
 
 if __name__ == "__main__":
 
-    new_password = "password1"
-    create_db(DB_PATH, new_password)
+    create_db(DB_PATH, DB_PASSWORD)
 
-    session = DatabaseEngine(DB_PATH, DB_PASSWORD)
+    db_session = DatabaseEngine(DB_PATH, DB_PASSWORD)
+    # print(db_session.get_all_accounts())
+    # print(db_session.get_account("http://www.wells.org/"))
 
-    print(session.get_all_accounts())
+    for i in range(35):
+        new_account = Account.from_faker()
+        db_session.add_account(new_account)
+        print(f"{new_account} added to database")
 
-    new_account = Account("ping.com", "username11", "password111")
-    new_account_2 = Account("sing.com", "username22", "password222")
+    # for el in db_session.get_all_accounts():
+    #     print(*el, sep="  --  ")
 
-    session.add_account(new_account)
-    session.add_account(new_account_2)
+    # print(db_session.get_account("and"))
 
-    res = session.get_all_accounts()
-    for el in res:
-        print(Account(*el[1:]))
+    db_session.safe_push()
+
+    # new_account = Account("ping.com", "username11", "password111")
+    # new_account_2 = Account("sing.com", "username22", "password222")
+
+    # session.add_account(new_account)
+    # session.add_account(new_account_2)
+
+    # res = session.get_all_accounts()
+    # for el in res:
+    #     print(Account(*el[1:]))
+
+    # session.safe_push()
